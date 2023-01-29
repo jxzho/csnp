ef76f6e375f823bc46dbe226cc98201f37208b80
russell <879917302@qq.com>
2023-01-29 19:01:46 +0800

    Update prompts.js

--- prompts.js
+++ prompts.js
@@ -1,30 +1,91 @@
+const fs = require('fs')
+const os = require('os')
+
 const prompts = require('prompts')
-const { lightGreen } = require('kolorist')
+const { cyan } = require('kolorist')
+
+const { currentOS } = require('./target-path')
+const { createCsnp } = require('./create-csnp')
+const { log } = require('./utils')
 
 const exec = async () => {
+  try {
+    const pathSnp = currentOS().pathSnippetsStored
 
-  const questions = [
-    {
-      type: 'text',
-      name: 'username',
-      message: 'What is your GitHub username?'
-    },
-    {
-      type: 'number',
-      name: 'age',
-      message: 'How old are you?'
-    },
-    {
-      type: 'text',
-      name: 'about',
-      message: 'Tell something about yourself',
-      initial: 'Why should I?'
+    if (pathSnp) {
+      const mode = await prompts({
+        type: 'confirm',
+        name: 'snippetCreate',
+        message: 'create new snippets',
+        initial: true
+      })
+
+      if (mode.snippetCreate) {
+        const res = await prompts([{
+          type: 'text',
+          name: 'type',
+          message: 'use snippet type',
+          initial: os.userInfo().username
+        }, {
+          type: 'text',
+          name: 'name',
+          message: 'input snippet name',
+          initial: 'my-snippets'
+        }, {
+          type: 'text',
+          name: 'prefix',
+          message: 'prefix for snippet?',
+        }])
+
+        const snpPath = `./csnp/${res.type}/${res.name}.csnp`
+
+        const created = await createCsnp(snpPath, {
+          name: res.name,
+          prefix: res.prefix
+        })
+
+        if (created) {
+          printLoveTips(snpPath)
+        } else {
+          log.error('create failed!')
+        }
+
+      } else {
+        let snippets = fs.readdirSync(pathSnp, 'utf-8')
+        snippets = snippets.filter(snp => {
+          return (/.+\.((code-snippets)|(json))$/.test(snp))
+        })
+        if (snippets.length) {
+          const res = await prompts({
+            type: 'select',
+            name: 'snippetUser',
+            message: 'select user snippets',
+            choices: snippets.map(snp => ({
+              value: snp,
+              title: /(.+)\..+$/.exec(snp)[1]
+            }))
+          })
+          console.log('=>> res', res.snippetUser)
+        }
+      }
     }
-  ]
+  } catch (error) {
+    throw Error(error)
+  }
 
-  const res = await prompts(questions)
+  // console.log(lightGreen(JSON.stringify(res, null, 2)))
+}
 
-  console.log(lightGreen(JSON.stringify(res, null, 2)))
+const printLoveTips = (snpPath) => {
+  console.log(`
+just use command 👇🏼
+
+${cyan(`\`vim ${snpPath}\``)}
+
+${cyan(`\`yarn run exec\``)}
+
+to generate your snippets! ❤️
+  `)
 }
 
 exec()
