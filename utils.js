1e22aad833e12a67c41633e3ed255aea65665367
russell <879917302@qq.com>
2023-01-29 19:00:19 +0800

    feat: change colors

--- utils.js
+++ utils.js
@@ -1,15 +1,15 @@
-const { yellow, lightGreen, red } = require('kolorist')
+const { lightYellow, lightGreen, lightRed } = require('kolorist')
 
 module.exports = {
   log: {
     info: (...args) => {
-      return console.log.apply(null, args.map(yellow))
+      return console.log.apply(null, args.map(lightYellow))
     },
     success: (...args) => {
       return console.log.apply(null, args.map(lightGreen))
     },
     error: (...args) => {
-      return console.log.apply(null, args.map(red))
+      return console.log.apply(null, args.map(lightRed))
     }
   }
 }
