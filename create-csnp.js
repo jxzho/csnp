8708b51cc0803fd64c7b601af7a89fdd93ccc401
钟俊雄 <zhongjunxiong@hdedu.com>
2023-01-29 19:01:44 +0800

    Create create-csnp.js

---
+++ create-csnp.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const fs = require('fs')
+const util = require('util')
+const matter = require('gray-matter')
+
+const mkdir = util.promisify(fs.mkdir)
+
+const createCsnp = (snpPath, data = {
+  name: '',
+  prefix: '',
+  description: '',
+  scope: ''
+}) => {
+  const strs = matter.stringify(`code snippets`, data)
+
+  const makeCsnp = val => fs.writeFileSync(snpPath, val, 'utf-8')
+
+  if (!fs.existsSync(snpPath)) {
+    const { dir } = path.parse(snpPath)
+    return mkdir(dir, { recursive: true }).then(() => {
+      makeCsnp(strs)
+      return true
+    })
+  }
+
+  return Promise.resolve(true)
+}
+
+module.exports = {
+  createCsnp
+}
