88cc3c9fbeba82d9ba48cafcc4ce3a53f843b8e8
钟俊雄 <zhongjunxiong@hdedu.com>
2023-01-31 20:57:44 +0800

    feat: rename `createCsnpLocal`

--- create-csnp.js
+++ create-csnp.js
@@ -5,27 +5,36 @@ const matter = require('gray-matter')
 
 const mkdir = util.promisify(fs.mkdir)
 
-const createCsnp = (snpPath, data = {
-  name: '',
-  prefix: '',
-  description: '',
-  scope: ''
-}) => {
-  const strs = matter.stringify(`code snippets`, data)
+const createCsnpLocal = (
+  snpPath,
+  data = {
+    name: '',
+    prefix: '',
+    description: '',
+    scope: '',
+  },
+  body = 'code snippets'
+) => {
+  const strs = matter.stringify(body, data)
 
-  const makeCsnp = val => fs.writeFileSync(snpPath, val, 'utf-8')
+  const makeCsnp = (val) => fs.writeFileSync(snpPath, val, 'utf-8')
 
-  if (!fs.existsSync(snpPath)) {
-    const { dir } = path.parse(snpPath)
-    return mkdir(dir, { recursive: true }).then(() => {
-      makeCsnp(strs)
-      return true
+  if (fs.existsSync(snpPath)) {
+    return Promise.resolve({
+      flag: false,
+      message: `${snpPath} file already exists!`
     })
   }
 
-  return Promise.resolve(false)
+  const { dir } = path.parse(snpPath)
+  return mkdir(dir, { recursive: true }).then(() => {
+    makeCsnp(strs)
+    return {
+      flag: true
+    }
+  })
 }
 
 module.exports = {
-  createCsnp
+  createCsnpLocal
 }
