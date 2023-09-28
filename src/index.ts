b9929c315844fa1a47be6008b1319750cfd18675
russell <879917302@qq.com>
2023-09-28 11:37:15 +0800

    feat: use shunt

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './utils/create-csnp'
 export * from './utils/csnp-to-vsc'
 export * from './utils/snippet-from-vsc'
+export * from './utils/shunt'
---
+++ src/utils/shunt.ts
@@ -0,0 +1,22 @@
+import minimist from 'minimist'
+import { putCsnpIntoVSC } from '../utils/csnp-to-vsc'
+import { syncCsnpFromVSC } from '../scripts/pull'
+import { Scope } from '../types/enums'
+
+type Continue = boolean
+
+export type Shunt = () => Promise<Continue>
+
+export const shunt = <Shunt>(async () => {
+  const argv = minimist(process.argv.slice(2))
+    
+  if (argv._?.[0] === 'push') {
+    putCsnpIntoVSC(argv.global ? Scope.GLOBAL : Scope.LOCAL)
+    return false
+  } else if (argv._?.[0] === 'pull') {
+    syncCsnpFromVSC(argv.global ? Scope.GLOBAL : Scope.LOCAL)
+    return false
+  }
+
+  return true
+})
